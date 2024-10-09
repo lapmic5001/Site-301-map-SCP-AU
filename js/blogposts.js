e9e86819187e433db9e627a2f692d32f0be92ce0
lapmic5001 <michaelvlaposta@gmail.com>
2024-10-09 13:12:24 -0400

    13.0.0-1

---
+++ js/blogposts.js
@@ -0,0 +1,99 @@
+const blogEntries = [
+    {
+        title: "(13) New bsft in dev! 10/9/2024 1:07 PM",
+        audio: "../audios/Kondraki's Theme_ King of Butterflies (SCP AU).mp3",
+        content: "Echo foundations complete. Will now work towards helix sector..."
+    },
+    {
+        title: "(12) MORE IDEAS :) 10/9/2024 10:43 AM",
+        images: [
+            "../images/pbpideaforpowerboxes.png",
+            "../images/scp106containmentideaplan.png"
+        ],
+        content: "I CAN'T STOP OMG"
+    },
+    {
+        title: "(11) Filling the 'surface' area 10/5/2024 5:00 PM",
+        images: [
+            "../screenshots/Screenshot 2024-10-05 165616.png"
+        ],
+        content: "added some windows to the surface. (If it's too much, dev team, YELL AT ME!)"
+    },
+    {
+        title: "(10) I got bored so Kondraki! 9/29/2024 10:37 PM",
+        images: [
+            "../gifs/RobloxStudioBeta_E1ywhjhe9D.gif"
+        ],
+        content: "So yeah, Kondraki exists now. (Don't mind the 12x4x12 Kondraki-Cube.)"
+    },
+    {
+        title: "(9) We got Walls! 9/24/2024 9:24 PM",
+        images: [
+            "../images/blenderimage--itstimetomakeassets!.png",
+            "../screenshots/Screenshot 2024-09-24 212148.png",
+            "../screenshots/Screenshot 2024-09-24 212205.png"
+        ],
+        content: "I TOLD YOU I WOULD ADD WALLS! also, 9:24 on 9/24 is crazy lol. The current walls have a lot of \"<span title='the space exists in an undefined state, without a clear future.'>limbo space</span>\"."
+    },
+    {
+        title: "(8) Audio Sneak Peaks 9/23/2024 10:47 PM",
+        audio: "../audios/Kondraki's Theme_ King of Butterflies (SCP AU).mp3",
+        content: "Sneak peek to some boss music, composed by your very own; <a href='https://onlinesequencer.net/members/142671'>Twinrova</a>!"
+    },
+    {
+        title: "(7) Concept art + funny 9/18/2024 9:45 PM",
+        images: [
+            "../images/floodlightsystemidea.png",
+            "../images/invsystemidea.png",
+            "../images/idkwtfthisisidea.png",
+            "../screenshots/Screenshot 2024-09-18 214321.png",
+            "../screenshots/Screenshot 2024-09-18 214421.png"
+        ],
+        content: "Stuff for inv system, floodlight system against eyes, and also a weird chemical flashlight I saw on <a href='https://www.youtube.com/watch?v=io8xRpadUoU'>yt shorts</a>."
+    },
+    {
+        title: "(6) Sentinel Floor complete 9/18/2024 8:55 PM",
+        images: [
+            "../screenshots/Screenshot 2024-09-18 210325.png",
+            "../screenshots/Screenshot 2024-09-18 205428.png"
+        ],
+        content: "Walls, again, will be added LATER, AAAAAAAAAAAAAAAAAAAAAAAAA<br>Abbreviations I will probably use:<br>bsft - Bossfight<br>rsch - Research<br>arms - Armory/Weapons<br>med - Medical"
+    },
+    {
+        title: "(5) Helix Floor complete 9/18/2024 8:17 PM",
+        images: [
+            "../screenshots/Screenshot 2024-09-18 201708.png",
+            "../screenshots/Screenshot 2024-09-18 201708-2.png"
+        ],
+        content: "Walls should be added soon, as well as a concept of what the finished product will be similar to."
+    },
+    {
+        title: "(4) Echo Foundation complete 9/18/2024 5:38 PM",
+        images: [
+            "../screenshots/Screenshot 2024-09-18 173740.png",
+            "../screenshots/Screenshot 2024-09-18 173748.png"
+        ],
+        content: "Echo foundations complete. Will now work towards helix sector..."
+    },
+    {
+        title: "(3) All Down from here... 9/18/2024 5:15 PM",
+        images: [
+            "../screenshots/Screenshot 2024-09-18 171515.png"
+        ],
+        content: "I am steadily making progress down to the <a href='site301map.html'>Echo</a> sector. Just building foundations right now. If you were to join, fly would probably be necessary."
+    },
+    {
+        title: "(2) Surface Walls Done! 9/18/2024 4:45 PM",
+        images: [
+            "../screenshots/Screenshot 2024-09-18 165359.png"
+        ],
+        content: "Progress is going great! I have the basic outlines for the <a href='site301surfacemap.html'>surface</a> built up in Roblox Studio!"
+    },
+    {
+        title: "(1) BEAUTIFUL PROGRESS 9/18/2024 4:30 PM",
+        images: [
+            "../screenshots/Screenshot 2024-09-18 162926.png"
+        ],
+        content: "Walls for the \"<a href='site301surfacemap.html'>surface</a>\" building are fully constructed in Roblox Studio!"
+    }
+];
